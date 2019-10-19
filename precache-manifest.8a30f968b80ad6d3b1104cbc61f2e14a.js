fe704b13d361347396363fc8cc2f44347582505a
Luchansky Oleg <luchansodev@gmail.com>
2019-10-19 13:04:05 +0300

    Updates

---
+++ precache-manifest.8a30f968b80ad6d3b1104cbc61f2e14a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2528562e2be0e6b659eb35f068608d47",
+    "url": "/invest-calculator/index.html"
+  },
+  {
+    "revision": "773c79a4ed56aa281cea",
+    "url": "/invest-calculator/static/css/2.d5d22505.chunk.css"
+  },
+  {
+    "revision": "6c7bd203303d3beaa3d9",
+    "url": "/invest-calculator/static/css/main.86efc044.chunk.css"
+  },
+  {
+    "revision": "773c79a4ed56aa281cea",
+    "url": "/invest-calculator/static/js/2.19096e77.chunk.js"
+  },
+  {
+    "revision": "6c7bd203303d3beaa3d9",
+    "url": "/invest-calculator/static/js/main.e957821c.chunk.js"
+  },
+  {
+    "revision": "a2eba633139b49d9770c",
+    "url": "/invest-calculator/static/js/runtime-main.59ccb2d0.js"
+  },
+  {
+    "revision": "9fdfa24f2ace08fd45da3821d1531479",
+    "url": "/invest-calculator/static/media/icon_close_l_black.9fdfa24f.svg"
+  },
+  {
+    "revision": "e81304e1ba9b00f38571aa084a3b5cfc",
+    "url": "/invest-calculator/static/media/icon_close_l_white.e81304e1.svg"
+  },
+  {
+    "revision": "88b9180ac58c35601d8425254044892c",
+    "url": "/invest-calculator/static/media/icon_close_m_black.88b9180a.svg"
+  },
+  {
+    "revision": "5d2c2756ab278b3f84735376af0c0284",
+    "url": "/invest-calculator/static/media/icon_close_m_white.5d2c2756.svg"
+  },
+  {
+    "revision": "af6676b2ef164647620b1e1ea84bb7c2",
+    "url": "/invest-calculator/static/media/icon_close_s_black.af6676b2.svg"
+  },
+  {
+    "revision": "cd06f8fbca7234fa3a10a5bc12a42205",
+    "url": "/invest-calculator/static/media/icon_close_s_white.cd06f8fb.svg"
+  },
+  {
+    "revision": "217cfbbc40165c8a3437e68dc59deca1",
+    "url": "/invest-calculator/static/media/icon_close_xl_black.217cfbbc.svg"
+  },
+  {
+    "revision": "0f90c8e88a2a481e64ef797a08517890",
+    "url": "/invest-calculator/static/media/icon_close_xl_white.0f90c8e8.svg"
+  },
+  {
+    "revision": "f5bc862ef625a947a5be6abc9d25c1ac",
+    "url": "/invest-calculator/static/media/icon_close_xs_black.f5bc862e.svg"
+  },
+  {
+    "revision": "bca604a95e3a09c97db710136b900ac3",
+    "url": "/invest-calculator/static/media/icon_close_xs_white.bca604a9.svg"
+  }
+]);
